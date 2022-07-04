690496b19bebf13b11db7b49cec5bebfdc4bd9b3
Nicolas Gonzalez <Nigonbol123@gmail.com>
2022-07-04 11:23:11 -0400

    recursion Nested array sum Solution [O(nk) guessing complexity]

--- specs/recursion/nested-arrays.test.js
+++ specs/recursion/nested-arrays.test.js
@@ -11,9 +11,19 @@
 
 function nestedAdd(array) {
   // write code here
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    const current = array[i];
+    if (Array.isArray(current)) {
+      sum += nestedAdd(current);
+    } else {
+      sum += current;
+    }
+  }
+  return sum;
 }
 
-test.skip("nested arrays addition", () => {
+test("nested arrays addition", () => {
   expect(nestedAdd([1, 2, 3])).toEqual(6);
   expect(nestedAdd([1, [2], 3])).toEqual(6);
   expect(nestedAdd([[[[[[[[[5]]]]]]]]])).toEqual(5);
