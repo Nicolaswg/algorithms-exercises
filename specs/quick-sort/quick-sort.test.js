d9f0f38a3a19eff5ccb4894d2eb5a09cbccb85eb
Nicolas Gonzalez <Nigonbol123@gmail.com>
2022-07-07 10:39:15 -0400

    quick sort solution for quick sort using a O(n log n ) complexity

--- specs/quick-sort/quick-sort.test.js
+++ specs/quick-sort/quick-sort.test.js
@@ -13,12 +13,37 @@
 */
 
 function quickSort(nums) {
-  // code goes here
+  // base case array of 0 or 1
+  if (nums.length <= 1) return nums;
+
+  // choose pivot
+  const pivot = nums[nums.length - 1];
+
+  // separate into left and right array
+  const left = [];
+  const right = [];
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] < pivot) {
+      left.push(nums[i]);
+    } else {
+      right.push(nums[i]);
+    }
+  }
+
+  // call quickSort on left and right array
+  // const sortedLeft = quickSort(left)
+  // const sortedRight = quickSort(right)
+
+  // // return the concat between left, pivot and right
+  // return sortedLeft.concat(pivot, sortedRight);
+
+  // more compact return
+  return [...quickSort(left), pivot, ...quickSort(right)];
 }
 
 // unit tests
 // do not modify the below code
-test.skip("quickSort", function () {
+test("quickSort", function () {
   const input = [10, 8, 2, 1, 6, 3, 9, 4, 7, 5];
   const answer = quickSort(input);
 
