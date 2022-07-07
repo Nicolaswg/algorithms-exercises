0fbca267ab749407c7cc52fa6eb873b44fba9e8f
Nicolas Gonzalez <Nigonbol123@gmail.com>
2022-07-07 13:37:05 -0400

    radix sort Solution for radix sort with a O(n k) complexity

--- specs/radix-sort/radix-sort.test.js
+++ specs/radix-sort/radix-sort.test.js
@@ -8,15 +8,48 @@
   it ends up being a lot more simple to implement.
 
 */
+function getDigit(number, place, longestNumber) {
+  const string = number.toString();
+  const size = string.length;
+
+  const mod = longestNumber - size;
+  return string[place - mod] || 0;
+}
+
+function findLongestNumber(array) {
+  let longest = 0;
+  for (let i = 0; i < array.length; i++) {
+    const currentLength = array[i].toString().length;
+    longest = currentLength > longest ? currentLength : longest;
+  }
+  return longest;
+}
 
 function radixSort(array) {
-  // code goes here
+  const longestNumber = findLongestNumber(array);
+
+  const buckets = new Array(10).fill().map(() => []); // make an array of 10 arrays
+
+  for (let i = longestNumber - 1; i >= 0; i--) {
+    while (array.length) {
+      const current = array.shift();
+      buckets[getDigit(current, i, longestNumber)].push(current);
+    }
+
+    for (let j = 0; j < 10; j++) {
+      while (buckets[j].length) {
+        array.push(buckets[j].shift());
+      }
+    }
+  }
+
+  return array;
 }
 
 // unit tests
 // do not modify the below code
 test("radix sort", function () {
-  test.skip("should sort correctly", () => {
+  test("should sort correctly", () => {
     const nums = [
       20,
       51,
